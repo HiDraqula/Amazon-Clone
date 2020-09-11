c59f26dd607868d1077a392c9cce8303771f40f8
HiDraqula <aniketaz190@gmail.com>
2020-09-11 23:19:32 +0530

    RM

--- functions/index.js
+++ functions/index.js
@@ -24,7 +24,7 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 
 // API routes
-app.get('/', (req, res) => res.status(200).send('hello world'))
+app.get('/', (req, res) => res.status(200).send('hello world 😁'))
 
 app.post('/payments/create', async (req, res) => {
   const total = req.query.total;
